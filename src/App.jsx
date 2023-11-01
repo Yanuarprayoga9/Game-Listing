7da2bd19fa893e49269a670a7b6fe893b01c8264
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-11-02 04:59:05 +0700

    implement : theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import './App.css'
 import Home from './Pages/Home'
 import Header from './Components/Header'
+import { ThemeContext } from './Context/ThemeContext'
 function App() {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState('dark')
 
   return (
-    <div className={`${theme} ${theme=="dark" ?'bg-[#121212]':null}`}>
-    <Header/>
-     <Home/>
-    </div>
+    <ThemeContext.Provider value={{ theme,setTheme }}>
+      <div className={`${theme} ${theme == "dark" ? 'bg-[#121212]' : null}`}>
+        <Header />
+        <Home />
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react';
-import logo from '../assets/code.jpg'
-import { HiMagnifyingGlass, HiMoon, HiMiniSun } from "react-icons/hi2";
+import { useContext, useState } from 'react';
+import { HiMagnifyingGlass, HiMoon, HiSun } from "react-icons/hi2";
+import { ThemeContext } from '../Context/ThemeContext';
 
 const Header = () => {
-    const [toggle, setToggle] = useState(false)
-    const handleToggle = () => {
-        setToggle(!toggle)
-    }
+    // const [toggle, setToggle] = useState(false)
+    // const handleToggle = () => {
+    //     setToggle(!toggle)
+    // }
+    const { theme, setTheme } = useContext(ThemeContext);
     return (
-        <div className='flex items-center p-3 '>
+        <div className={`${theme=='dark'? 'text-lime-50': ''} flex items-center p-3`}>
             {/* <img src={logo} alt="logo" width={60}/> */}
-            <h3>Yanuar</h3>
+            <h2 >Yanuar</h2>
             <div className='flex bg-slate-300 p-2 w-full items-center mx-5 rounded-full mt-2 m-2'>
                 <HiMagnifyingGlass />
-                <input type="text" name="" id="" className='px-2 dark:bg-rose-800 bg-transparent outline-none' placeholder='Search Games' />
+                <input type="text" name="" id="" className='px-2  bg-transparent outline-none' placeholder='Search Games' />
             </div>
-            <div className="">
-                <button onClick={handleToggle}>{toggle ? <HiMoon className='bg-slate-300 rounded-full text-[30px] text-black p-1' /> : <HiMiniSun className='bg-slate-300 rounded-full text-[30px] text-black p-1' />}</button>
+            <div>
+                {theme == 'dark' ? <HiSun className="text-[35px] cursor-pointer bg-gray-200 text-black p-1 rounded-full "
+                    onClick={() => setTheme('light')} />
+                    :
+                    <HiMoon className="text-[35px] cursor-pointer bg-gray-200 text-black p-1 rounded-full"
+                        onClick={() => setTheme('dark')} />}
             </div>
         </div>
 
