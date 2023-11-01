287f3e5404954f11a7a94ee0b2fc4737f50810fc
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-11-01 19:57:29 +0700

    add : header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-
 import './App.css'
-
+import Home from './Pages/Home'
+import Header from './Components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
+     <Home/>
     </>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import logo from '../assets/code.jpg'
+import { HiMagnifyingGlass, HiMoon, HiMiniSun } from "react-icons/hi2";
+
+const Header = () => {
+    const [toggle, setToggle] = useState(false)
+    const handleToggle = () => {
+        setToggle(!toggle)
+    }
+    return (
+        <div className='flex items-center'>
+            {/* <img src={logo} alt="logo" width={60}/> */}
+            <h3>Yanuar</h3>
+            <div className='flex bg-slate-300 p-2 w-full items-center mx-5 rounded-full mt-2 m-2'>
+                <HiMagnifyingGlass />
+                <input type="text" name="" id="" className='px-2 bg-transparent outline-none' placeholder='Search Games' />
+            </div>
+            <div className="">
+                <button onClick={handleToggle}>{toggle ? <HiMoon className='bg-slate-300 rounded-full text-[30px] text-black p-1' /> : <HiMiniSun className='bg-slate-300 rounded-full text-[30px] text-black p-1' />}</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header
