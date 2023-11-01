b6ca839bacf00b9fc335e7ce0e0f18599faad54c
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-11-01 21:20:55 +0700

    add : implement darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import Home from './Pages/Home'
 import Header from './Components/Header'
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('light')
 
   return (
-    <>
+    <div className={`${theme} ${theme=="dark" ?'bg-[#121212]':null}`}>
     <Header/>
      <Home/>
-    </>
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
         setToggle(!toggle)
     }
     return (
-        <div className='flex items-center'>
+        <div className='flex items-center p-3 '>
             {/* <img src={logo} alt="logo" width={60}/> */}
             <h3>Yanuar</h3>
             <div className='flex bg-slate-300 p-2 w-full items-center mx-5 rounded-full mt-2 m-2'>
                 <HiMagnifyingGlass />
-                <input type="text" name="" id="" className='px-2 bg-transparent outline-none' placeholder='Search Games' />
+                <input type="text" name="" id="" className='px-2 dark:bg-rose-800 bg-transparent outline-none' placeholder='Search Games' />
             </div>
             <div className="">
                 <button onClick={handleToggle}>{toggle ? <HiMoon className='bg-slate-300 rounded-full text-[30px] text-black p-1' /> : <HiMiniSun className='bg-slate-300 rounded-full text-[30px] text-black p-1' />}</button>
